001c9f2925213574619559fa90aec50d0441a2fb
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-07-19 17:14:00 +0100

    Addition of loading spinner

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 function LoadingPage() {
   return (
-    <Box sx={{ bgcolor: "#fff", height:"100%", width:"100%", position:"fixed" }}><Typography variant="h4" sx={{textAlign:"center", p: 10}} >Finding your city...</Typography></Box>
-  )
+    <Box
+      sx={{ bgcolor: "#fff", height: "100%", width: "100%", position: "fixed" }}
+    >
+      <Typography variant="h4" sx={{ textAlign: "center", p: 10 }}>
+        Finding your city...
+      </Typography>
+      <div className="text-center">
+        <div className="spinner-border" role="status"></div>
+      </div>
+    </Box>
+  );
 }
 
-export default LoadingPage
+export default LoadingPage;
