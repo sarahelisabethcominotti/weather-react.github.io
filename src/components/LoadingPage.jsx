adce264f91af5973c4c02ee2bb671ae560990d89
sarahelisabethcominotti <138233386+sarahelisabethcominotti@users.noreply.github.com>
2024-09-04 14:00:50 +0200

    update of loading page

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
+import WaterDropIcon from "@mui/icons-material/WaterDrop";
+import { Water } from "@mui/icons-material";
 
 function LoadingPage() {
   return (
-    <Box
-      sx={{ bgcolor: "#fff", height: "100%", width: "100%", position: "fixed" }}
-    >
-      <Typography variant="h4" sx={{ textAlign: "center", p: 10 }}>
+    <Box sx={{ bgcolor: "rgba(224, 247, 250, 0.7)", padding: "100px" }}>
+      <Typography
+        variant="h4"
+        sx={{ textAlign: "center", paddingBottom: "10px" }}
+      >
         Finding your city...
       </Typography>
-      <div className="text-center">
+      {/* <div className="text-center">
         <div className="spinner-border" role="status"></div>
+      </div> */}
+      <div className="loader-container">
+        <div className="bouncing-drops">
+          <div className="drop"><WaterDropIcon color="primary"/></div>
+          <div className="drop"><WaterDropIcon color="primary"/></div>
+          <div className="drop"><WaterDropIcon color="primary"/></div>
+        </div>
       </div>
     </Box>
   );
